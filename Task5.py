fdbac7ff6dc127cc3c654599cca52aada9998916
alyanna-abz <alyannaocenar6@gmail.com>
2025-02-21 14:33:58 +0800

    Create Task5.py

---
+++ Task5.py
@@ -0,0 +1,17 @@
+class Employee:
+    def __init__(self, name, position, salary):
+        self.name = name
+        self.position = position
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+
+    def display_employee(self):
+        print(f"Employee Name: {self.name}")
+        print(f"Position: {self.position}")
+        print(f"Salary: Php{self.salary}")
+        
+employee1 = Employee("Terry Mark", "Web Designer", 27000)
+employee1.give_raise(5000)
+employee1.display_employee()
